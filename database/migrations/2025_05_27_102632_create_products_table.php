3b24cb8ae4f9f5d3e5cab2f6d97c6e526400b2d2
Daud Raza <daud.raza@conovoinc.com>
2025-05-27 15:58:29 +0500

    migration created for products & reviews

--- database/migrations/2025_05_27_102632_create_products_table.php
+++ database/migrations/2025_05_27_102632_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
+            $table->text('detail')->nullable();
+            $table->decimal('price', 10, 2)->nullable();
+            $table->decimal('stock', 10, 2)->nullable();
+            $table->decimal('discount', 10, 2)->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2025_05_27_102648_create_reviews_table.php
+++ database/migrations/2025_05_27_102648_create_reviews_table.php
@@ -12,7 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('reviews', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->unsignedBigInteger('product_id')->index();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->string('customer')->nullable();
+            $table->text('review')->nullable();
+            $table->integer('star')->nullable();
             $table->timestamps();
         });
     }
